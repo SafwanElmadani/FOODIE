68505c5599b6841d5a459a137da3d76f680ef0d9
safwanelmadani <safwanelmadani@arizona.edu>
2023-04-30 17:35:01 -0700

    update arch

--- FOODIE_BAGGER.py
+++ FOODIE_BAGGER.py
@@ -1,15 +1,15 @@
 
 
 observed_data = {
-    "loaf of bread": [1, "dry_food"],
-    "eggs": [2,"dry_food", "fragile"], 
-    "bag of onions": [9,"dry_food"], 
-    "chips": [0.5,"dry_food", "fragile"],
-    "meat": [7,"leaky_food"],
-    "gallon of milk": [8.6,"leaky_food"],
-    "gallon of water": [8.3,"leaky_food"],
-    "watermelon": [9,"dry_food"],
-    "pasta": [1.1,"dry_food"],
+    "loaf of bread": [1],
+    "eggs": [2, "fragile"], 
+    "bag of onions": [9], 
+    "chips": [0.5,"fragile"],
+    "meat": [7],
+    "gallon of milk": [8.6],
+    "gallon of water": [8.3],
+    "watermelon": [9],
+    "pasta": [1.1],
     "pint of ice cream": [1.04,"frozen_food"],
 
 }
@@ -24,62 +24,74 @@ bags_count = 0
 
 def rules():
     global working_memory
+    global current_bag_items
     global bags_count
-    #bag frozen items R1
-    for item in order:
-        if "frozen_food" in observed_data[item[0]]:
-            print(f"Rule 1 applies: bag {item[0]} in the freezer bag")
-            order.remove(item)
-    #need to bag large items first
-    for item in order:
+    print(f"*** start bagging ***")
+    while True:
+        #bag frozen items R1
+        for item in order:
+            if "frozen_food" in observed_data[item[0]]:
+                print(f"Rule 1 applies: bag {item[0]} in the freezer bag")
+                order.remove(item)
+        #need to bag large items first
+        for item in order:
 
-        #check for large items R2
-        if observed_data[item[0]][0] > 8:
-            #print(item[0], "is a large item")
-            working_memory = working_memory + [item[0]] + observed_data[item[0]]
-            working_memory.append("large")
-            print(f"Rule 2 applies: bagging a large item")
+            #check for large items R2
+            if observed_data[item[0]][0] > 8:
+                #print(item[0], "is a large item")
+                working_memory = working_memory + [item[0]] + observed_data[item[0]]
+                working_memory.append("large")
+                print(f"Rule 2 applies: bagging a large item")
 
-        #check for medioum items R3
-        if 5 <= observed_data[item[0]][0] <= 8:
-            working_memory = working_memory + [item[0]] + observed_data[item[0]]
-            working_memory.append("medioum")
-            print(f"Rule 3 applies: bagging a medioum item")
+            #check for medioum items R3
+            if 5 <= observed_data[item[0]][0] <= 8:
+                working_memory = working_memory + [item[0]] + observed_data[item[0]]
+                working_memory.append("medioum")
+                print(f"Rule 3 applies: bagging a medioum item")
 
-        #check for small items R4
-        if observed_data[item[0]][0] < 5:
-            working_memory = working_memory + [item[0]] + observed_data[item[0]]
-            working_memory.append("small")
-            print(f"Rule 4 applies: bagging a small item")
+            #check for small items R4
+            if observed_data[item[0]][0] < 5:
+                working_memory = working_memory + [item[0]] + observed_data[item[0]]
+                working_memory.append("small")
+                print(f"Rule 4 applies: bagging a small item")
 
-        #check if we need a new bag for large item R5
-        if "large" in working_memory and current_bag_items == 1:
-            working_memory.append("need_new_bag")
+            #check if we need a new bag for large item R5
+            if "large" in working_memory and current_bag_items == 1:
+                working_memory.append("need_new_bag")
 
-        #check if we need a new bag for medioum item R6
-        if "medioum" in working_memory and current_bag_items == 5:
-            working_memory.append("need_new_bag")
+            #check if we need a new bag for medioum item R6
+            if "medioum" in working_memory and current_bag_items == 5:
+                working_memory.append("need_new_bag")
 
-        #check if we need a new bag for small items R7
-        if "small" in working_memory and current_bag_items == 10:
-            working_memory.append("need_new_bag")
+            #check if we need a new bag for small items R7
+            if "small" in working_memory and current_bag_items == 10:
+                working_memory.append("need_new_bag")
 
-        #check if a new paper bag is needed R8
-        if "dry_food" in working_memory and "need_new_bag" in working_memory:
-            print(f"Rule R8 applies: starting a new paper bag")
-            bags_count = bags_count + 1
+            #check if a new paper bag is needed R8
+            if "need_new_bag" in working_memory:
+                print(f"Rule R8 applies: starting a new bag")
+                bags_count = bags_count + 1
 
-       #check if a new plastic bag is needed R9 
-        if "leaky_food" in working_memory and "need_new_bag" in working_memory:
-            print(f"Rule R9 applies: starting a new plastic bag")
-            bags_count = bags_count + 1
-        
-        #bag large item R10
-        if "large" in working_memory and current_bag_items < 1:
-            print(f"Rule R10 applies: put {item[0]} in bag_")
-            
+            #bag large item R9
+            if "large" in working_memory and current_bag_items < 1:
+                print(f"Rule R9 applies: put {item[0]} in bag_{bags_count}")
+                current_bag_items = current_bag_items + 1
+                
+            #bag medioum item R10
+            if "medioum" in working_memory and current_bag_items < 5:
+                print(f"Rule R10 applies: put {item[0]} in bag_{bags_count}")
 
+            #bag medioum item R11
+            if "small" in working_memory and current_bag_items < 10:
+                print(f"Rule R11 applies: put {item[0]} in bag_{bags_count}")
 
+            #bag fragile items last R12
+            if "fragile" in working_memory:
+                print(f"Rule R12 applies: put {item[0]} in a separete bag and place it on top of other bags")
+
+        if not order:
+            print(f"*** bagging is finished ***")
+            break
 
 if __name__ == "__main__":
     rules()
